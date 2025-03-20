2434e3ff002c43382176c51e9202a74a96ec4c95
glomdom <22115@uktc-bg.com>
2025-03-20 20:17:06 +0200

    chore: reorganize files

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use card::{load_cards_from_file, parse_carddefs_xml, save_cards_to_file};
 use log::{LevelFilter, info};
+use serialization::{load_cards_from_file, save_cards_to_file};
 use std::time::Instant;
+use xml_parser::parse_carddefs_xml;
 
 mod card;
+mod serialization;
+mod xml_parser;
 
 fn main() {
     env_logger::builder()
@@ -13,7 +16,11 @@ fn main() {
     let parse_start = Instant::now();
     let defs = parse_carddefs_xml("hsdata/CardDefs.xml", "enUS");
     let parse_duration = parse_start.elapsed();
-    info!("finished parsing {} cards from `hsdata/CardDefs.xml` in {:.2?}", defs.cards.len(), parse_duration);
+    info!(
+        "finished parsing {} cards from `hsdata/CardDefs.xml` in {:.2?}",
+        defs.cards.len(),
+        parse_duration
+    );
 
     let save_start = Instant::now();
     save_cards_to_file(&defs, "cards.dat").expect("failed to save cards to file");
@@ -23,7 +30,11 @@ fn main() {
     let load_start = Instant::now();
     let loaded_defs = load_cards_from_file("cards.dat").expect("failed to load cards from file");
     let load_duration = load_start.elapsed();
-    info!("read {} cards in {:.2?}", loaded_defs.cards.len(), load_duration);
+    info!(
+        "read {} cards in {:.2?}",
+        loaded_defs.cards.len(),
+        load_duration
+    );
 
     info!("parsing took: {:.2?}", parse_duration);
     info!("saving took: {:.2?}", save_duration);
